4f5a799c2cc33da1ab864746e1180fd0d6ca0770
Niels G. W. Serup <ngws@metanohi.name>
2016-10-10 22:45:14 +0200

    Gather all data in a master directory.

---
+++ src/gather-data.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+set -e # Exit on first error.
+
+pacdir="$1"
+outdir="$2"
+
+if ! [ "$pacdir" ]; then
+    echo 'error: specify a package directory'
+    exit 1
+fi
+
+if ! [ "$outdir" ]; then
+    echo 'error: specify an output directory'
+    exit 1
+fi
+
+base="$(readlink -f "$(dirname "$0")")"
+
+outdir="$(readlink -f "$outdir")"
+
+mkdir -p "$outdir"
+
+stack ghc -- -O3 $base/get-description.hs
+
+cd "$pacdir"
+for dir in *; do
+    name="$(echo "$dir" | sed -r 's/-[^-]+$//')"
+    version="$(echo "$dir" | grep -Eo '[^-]+$')"
+    cabal_file="$(echo "$dir/"*.cabal)"
+    resdir="$outdir/$name"
+    echo $name
+    mkdir "$resdir"
+    echo $name > "$resdir/name"
+    echo $version > "$resdir/version"
+    "$base/get-imports.sh" "$dir" > "$resdir/imports"
+    "$base/get-description" "$cabal_file" | {
+        read modules
+        read dependencies
+        echo $modules > "$resdir/modules"
+        echo $dependencies > "$resdir/dependencies"
+    }
+done
