cba95c71239f0144712cce3de3cd510961856e2f
Niels G. W. Serup <ngws@metanohi.name>
2016-10-10 22:59:41 +0200

    imports simplification fix

--- src/get-imports.sh
+++ src/get-imports.sh
@@ -14,7 +14,7 @@ find "$dir" -name '*.hs' \
     | \
     {
         while read filename; do
-            grep -E '^import\s+(qualified)?\s*[A-Z]' "$filename" \
+            grep -Eo '^import\s+(qualified)?\s*([A-Z][A-Za-z]*\.?)+' "$filename" \
                 | sed 's/ qualified//' \
                 | cut -d' ' -f2
         done
